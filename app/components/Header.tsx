c87753e069178c19d01402a6b944ee2e3b67ed0d
vdeekshithkumar <DeekshithK@ivoyant.com>
2024-10-18 11:45:28 +0530

    inital commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+import Nav from "./Nav";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 z-[1] mx-auto  flex w-full flex-wrap items-center justify-between border-b border-gray-100 bg-background p-[2em] font-sans font-bold uppercase text-text-primary backdrop-blur-[100px] dark:border-gray-800 dark:bg-d-background dark:text-d-text-primary">
+      {/* <Logo /> */}
+      DKV Photography
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
