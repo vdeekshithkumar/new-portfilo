5a9f03e1fb461cde39715471b3396345c90d4f73
deekshithkumarv <vdeekshithkumar@gmail.com>
2025-02-19 10:22:31 +0530

    updated code

--- remix.config.js
+++ remix.config.js
@@ -18,5 +18,5 @@ module.exports = {
   assetsBuildDirectory: "public/build",
   serverBuildPath: "build/index.js",
   publicPath: "/build/",
-  serverBuildTarget: "vercel",
+  serverBuildTarget: "netlify",
 };
